ca933a07a9e8af84d37e2b1e4a68d17d923e4a9e
Aderowski <aderowski03@gmail.com>
2021-10-08 21:15:01 +0200

    Pierwsza wrzutka

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # praktyki_api
-Zaprogramowanie strony z API - forma praktyki zawodowej 2021
+## Zaprogramowanie strony z API - forma praktyki zawodowej 2021
+## Praktyka rozpoczęta dnia 04.10.2021
---
+++ resources/js/btn_top.js
@@ -0,0 +1,21 @@
+let przycisk = document.querySelector("#btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
+        przycisk.style.display = "block";
+    else
+        przycisk.style.display = "none";
+
+}
+// When the user clicks on the button, scroll to the top of the document
+przycisk.addEventListener("click", backToTop);
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
